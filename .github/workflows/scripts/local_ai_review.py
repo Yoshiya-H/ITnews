98a78cd6e5af7c75af4fd9874ffc763d5cf1b758
Yoshiya <hmntmy@yahoo.co.jp>
2025-06-22 01:27:43 +0900

    [Fix] Fix get diff.

--- .github/workflows/scripts/local_ai_review.py
+++ .github/workflows/scripts/local_ai_review.py
@@ -1,21 +1,11 @@
-import subprocess
 import os
 from transformers import pipeline
 
-def get_code_diff():
-    """
-    現在のプルリクエストのコード差分を取得    
-    """
-    result = subprocess.run([
-        'git', 'diff', 
-        os.environ['BASE_SHA'],
-        os.environ['HEAD_SHA']
-    ], capture_output=True, text=True, check=False)
-    return result.stdout
 
 reviewer = pipeline("text-generation", model="Salesforce/codegen-350M-mono")
-diff = get_code_diff()
+diff = os.environ['DIFF']
 print(os.environ['BASE_SHA'])
+print('->')
 print(os.environ['HEAD_SHA'])
 print(f'diff: {diff}')
 
