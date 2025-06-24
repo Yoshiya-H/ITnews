4d477f59ee0b44e4decfb687f9b7d21988206637
Yoshiya <hmntmy@yahoo.co.jp>
2025-06-24 15:04:56 +0900

    [Fix] Change to get diff.

--- .github/workflows/scripts/local_ai_review.py
+++ .github/workflows/scripts/local_ai_review.py
@@ -6,10 +6,10 @@ from openai import OpenAI
 
 ai_api_key = os.getenv("AI_API_KEY")
 
-diff = sys.argv[1]
+#diff = sys.argv[1]
 
 SYSTEM_PROMPT = "You are a professional infrastructure engineer who is good at code reviews. When reviewing code, please try to show the source whenever possible."
-USER_PROMPT = f"Please review the following code differences in Japanese. Please output the good points and improvements.:\n\n{diff}"
+USER_PROMPT = "Please review the following code differences in Japanese. Please output the good points and improvements.:\n\n"
 MODEL= "gemini-1.5-flash-lite"
 
 GITHUB_TOKEN = os.getenv('GH_TOKEN')
@@ -17,10 +17,21 @@ REPOSITORY = os.getenv("REPOSITORY")
 PR_NUMBER = int(os.getenv("PR_NUMBER"))
 PR_API_URL = f'https://api.github.com/repos/{REPOSITORY}/pulls/{PR_NUMBER}'
 
-def get_ai_review():
+def get_pr_diff():
+    """PRの差分を取得"""
+    url = f'{PR_API_URL}/files'
+    headers = {
+        'Authorization': f'token {GITHUB_TOKEN}',
+        'Accept': 'application/vnd.github+json'
+    }
+    response = requests.get(url, headers=headers)
+    pr_diff = response.json()
+    return pr_diff
+
+def get_ai_review(diff):
     """AIによるコードレビュー"""
     client = OpenAI(
-        api_key="AI_API_KEY",
+        api_key=ai_api_key,
         base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
     )
 
@@ -28,7 +39,7 @@ def get_ai_review():
         model=MODEL,
         messages=[
             {"role": "system", "content": SYSTEM_PROMPT},
-            {"role": "user", "content": USER_PROMPT}
+            {"role": "user", "content": f'{USER_PROMPT}{diff}'}
         ],
         response_format={"type":"json_object"}
     )
@@ -56,5 +67,6 @@ def post_review_comments(review_result):
             }
             requests.post(comment_url, headers=headers, data=json.dumps(comment_data))
 
-review_result = get_ai_review()
+diff = get_pr_diff()
+review_result = get_ai_review(diff)
 post_review_comments(json.loads(review_result))
