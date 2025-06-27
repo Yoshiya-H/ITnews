c96f79af185e287ce6828bef45b8b8b02056c41a
Yoshiya <hmntmy@yahoo.co.jp>
2025-06-27 15:20:38 +0900

    [Add] Change the front design using php

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from rss_util import get_news
 
 app = Flask(__name__)
@@ -8,5 +8,10 @@ def index():
     news = get_news()
     return render_template('index.html', news=news)
 
+@app.route('/api/news')
+def api_news():
+    news = get_news()
+    return jsonify(news)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
