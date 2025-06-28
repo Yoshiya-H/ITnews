4dee2c6103d17c71cd0fc50618bd9d21f01ec84d
Yoshiya <hmntmy@yahoo.co.jp>
2025-06-29 03:01:12 +0900

    [Fix] Change frontend design

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@
 resource "aws_ecr_repository" "python_app_repo" {
   name = var.python_ecr_repo_name
   image_tag_mutability = "MUTABLE"
-
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -12,7 +12,7 @@ resource "aws_ecr_repository" "python_app_repo" {
 resource "aws_ecr_repository" "php_app_repo" {
   name = var.php_ecr_repo_name
   image_tag_mutability = "MUTABLE"
-
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
