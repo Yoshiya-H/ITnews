67a4b795e562f64b853cdac552d076a14a49bec0
Yoshiya <hmntmy@yahoo.co.jp>
2025-06-30 05:42:36 +0900

    [Fix] Changed to allow commands to be executed within ECS

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from rss_util import get_news
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    news = get_news()
-    return render_template('index.html', news=news)
+# @app.route('/')
+# def index():
+#     news = get_news()
+#     return render_template('index.html', news=news)
 
 @app.route('/api/news')
 def api_news():
